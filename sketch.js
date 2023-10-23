73d9d3c2b52bb49ba87b512a25a07e72ca5b2d6c
killopro1234 <98070069+killopro1234@users.noreply.github.com>
2023-10-23 16:52:43 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground;
+var fruit,rope;
+var fruit_con;
+
+
+function preload(){
+  bg_img = loadImage('fondo.jpg');
+  food_img = loadImage('R.jpeg');
+  patricio_img = loadImage('1PA.gif');
+}
+
+function setup() 
+{
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(200,680,600,20);
+
+  rope = new Rope(7,{x:245,y:30});
+  fruit = Bodies.circle(300,300,20);
+  Matter.Composite.add(rope.body,fruit);
+
+  fruit_con = new Link(rope,fruit);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+ 
+  
+}
+
+function draw() 
+{
+  background(51);
+  image(bg_img,0,0,displayWidth+80,displayHaight);
+  rope.show();
+  ellipse(fruit.position.x,fruit.position.y,30,30);
+  Engine.update(engine);
+  ground.show();
+
+}
